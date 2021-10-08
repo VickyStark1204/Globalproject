d2912e74febc13dd569fc216c7a20d103c48e521
VickyStark1204 <cvignesh124@gmail.com>
2021-10-08 14:34:03 +0530

    passWord

---
+++ src/org/libGlobal/Test2.java
@@ -0,0 +1,14 @@
+package org.libGlobal;
+
+public class Test2 {
+
+	public static void passWord() {
+
+		System.out.println("PassWord");
+	}
+
+	public static void main(String[] args) {
+		passWord();
+	}
+
+}
