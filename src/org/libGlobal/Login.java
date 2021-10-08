c5524f148680287882bffa293673e8809d87813d
VickyStark1204 <cvignesh124@gmail.com>
2021-10-08 20:18:22 +0530

    Login Repo

---
+++ src/org/libGlobal/Login.java
@@ -0,0 +1,15 @@
+package org.libGlobal;
+
+public class Login {
+
+	public static void btnlogin() {
+		
+		System.out.println("Login Button");
+	}
+
+	public static void main(String[] args) {
+		btnlogin();
+	}
+
+	}
+
