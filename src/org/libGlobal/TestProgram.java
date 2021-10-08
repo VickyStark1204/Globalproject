a6649d7504a381255bd4e3c2268d6e0c0f530b25
VickyStark1204 <cvignesh124@gmail.com>
2021-10-08 14:00:11 +0530

    userName

--- src/org/libGlobal/TestProgram.java
+++ src/org/libGlobal/TestProgram.java
@@ -1,5 +1,12 @@
 package org.libGlobal;
 
 public class TestProgram {
+	
+	public static void UserName() {
+		System.out.println("UserName");
+	}
+	public static void main(String[] args) {
+		UserName();
+	}
 
 }
